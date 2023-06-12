cd8e61832ca6bdcd8c9d36bbbea290f63405eea1
Damian Melia <113540164+meliadamian17@users.noreply.github.com>
2023-06-11 20:02:17 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -6,47 +6,33 @@ import java.util.Arrays;
 
 public class Driver {
 	public static void main(String [] args) throws IOException {
-//		Polynomial p = new Polynomial();
-//		System.out.println(p.evaluate(3));
-//		double [] c1 = {6,5};
-//		int[] po1 = {0,3};
-//		Polynomial p1 = new Polynomial(c1, po1);
-//		double [] c2 = {-2,-9};
-//		int[] po2 = {1,4};
-//		Polynomial p2 = new Polynomial(c2, po2);
-//		Polynomial s = p1.add(p2);
-//		System.out.println("s(0.1) = " + s.evaluate(0.1));
-//		if(s.hasRoot(1)) {
-//			System.out.println("1 is a root of s");
-//		} else {
-//			System.out.println("1 is not a root of s");
-//		}
-		double[] coeff1 = {2, 4, 1, 5};
+
+		double[] coeff1 = {1, 2, 4, 7};
 		int[] pow1 = {0, 1, 2, 3};
 		Polynomial poly1 = new Polynomial(coeff1, pow1);
 
 		double[] coeff2 = {2, 2};
-		int[] pow2 = {0, 3};
+		int[] pow2 = {2, 4};
 		Polynomial poly2 = new Polynomial(coeff2, pow2);
 
-		double[] coeff3 = {1, 1, 2, 2, 1, 1};
-		int[] pow3 = {2, 3, 4, 5, 6, 7};
+		double[] coeff3 = {1, 3, 3, 4, 5, 1};
+		int[] pow3 = {2, 3, 4, 5, 6, 9};
 		Polynomial poly3 = new Polynomial(coeff3, pow3);
 
 
-		System.out.println("---------------NEW TESTS---------------");
+		System.out.println("Test Cases:");
 
 		System.out.println("Test Evaluate:");
 		System.out.println("Received: " +  poly1.evaluate(3));
-		System.out.println("Expected: 158.0");
+		System.out.println("Expected: 232.0");
 		System.out.println();
 
 		System.out.println("Received: " +  poly2.evaluate(2));
-		System.out.println("Expected: 18.0");
+		System.out.println("Expected: 40.0");
 		System.out.println();
 
 		System.out.println("Received: " +  poly3.evaluate(1));
-		System.out.println("Expected: 8.0");
+		System.out.println("Expected: 17.0");
 		System.out.println();
 
 		System.out.println("Test HasRoot:");
@@ -55,43 +41,35 @@ public class Driver {
 		System.out.println();
 
 		System.out.println("Received: " +  poly2.hasRoot(-1));
-		System.out.println("Expected: true");
-		System.out.println();
-
-		System.out.println("Received: " +  poly2.hasRoot(-1));
-		System.out.println("Expected: true");
+		System.out.println("Expected: false");
 		System.out.println();
 
 		System.out.println("Test Add:");
-		Polynomial sum =  poly2.add(poly1);
-		System.out.println("Received: " + Arrays.toString(sum.coefficients) + "  " + Arrays.toString(sum.exponents));
-		System.out.println("Expected: [4.0, 4.0, 1.0, 7.0]  [0, 1, 2, 3]");
-		System.out.println();
 
 		Polynomial sum2 = poly1.add(poly2);
 		System.out.println("Received: " +  Arrays.toString(sum2.coefficients) + "  " + Arrays.toString(sum2.exponents));
-		System.out.println("Expected: [4.0, 4.0, 1.0, 7.0]  [0, 1, 2, 3]");
+		System.out.println("Expected: [1.0, 2.0, 6.0, 7.0, 2.0]  [0, 1, 2, 3, 4]");
 		System.out.println();
 
 		Polynomial sum3 = poly1.add(poly3);
 		System.out.println("Received: " +  Arrays.toString(sum3.coefficients) + "  " + Arrays.toString(sum3.exponents));
-		System.out.println("Expected: [2.0, 4.0, 2.0, 6.0, 2.0, 2.0, 1.0, 1.0]  [0, 1, 2, 3, 4, 5, 6, 7]");
+		System.out.println("Expected: [1.0, 2.0, 5.0, 10.0, 3.0, 4.0, 5.0, 1.0]  [0, 1, 2, 3, 4, 5, 6, 9]");
 		System.out.println();
 
 		System.out.println("Test Multiply:");
 		Polynomial prod1 =  poly1.multiply(poly2);
 		System.out.println("Received: " + Arrays.toString(prod1.coefficients) + "  " + Arrays.toString(prod1.exponents));
-		System.out.println("Expected: [4.0, 8.0, 2.0, 14.0, 8.0, 2.0, 10.0]  [0, 1, 2, 3, 4, 5, 6]");
+		System.out.println("Expected: [2.0, 4.0, 10.0, 18.0, 8.0, 14.0]  [2, 3, 4, 5, 6, 7]");
 		System.out.println();
 
 		Polynomial prod2 = poly2.multiply(poly1);
 		System.out.println("Received: " +  Arrays.toString(prod2.coefficients) + "  " + Arrays.toString(prod2.exponents));
-		System.out.println("Expected: [4.0, 8.0, 2.0, 14.0, 8.0, 2.0, 10.0]  [0, 1, 2, 3, 4, 5, 6]");
+		System.out.println("Expected: [2.0, 4.0, 10.0, 18.0, 8.0, 14.0]  [2, 3, 4, 5, 6, 7]");
 		System.out.println();
 
 		Polynomial prod3 = poly1.multiply(poly3);
 		System.out.println("Received: " +  Arrays.toString(prod3.coefficients) + "  " + Arrays.toString(prod3.exponents));
-		System.out.println("Expected: [2.0, 6.0, 9.0, 18.0, 17.0, 18.0, 15.0, 6.0, 5.0]  [2, 3, 4, 5, 6, 7, 8, 9, 10]");
+		System.out.println("Expected: [1.0, 5.0, 13.0, 29.0, 46.0, 47.0, 48.0, 36.0, 2.0, 4.0, 7.0]  [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]");
 		System.out.println();
 
 		System.out.println("Test Read from File:");
@@ -119,6 +97,6 @@ public class Driver {
 		} catch (IOException e) {
 			System.err.println("Error reading the file: " + e.getMessage());
 		}
-		System.out.println("Expected: 2.0+4.0x+1.0x2+5.0x3");
+		System.out.println("Expected: 1.0+2.0x1+4.0x2+7.0x3");
 	}
 }
